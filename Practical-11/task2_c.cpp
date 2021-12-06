b9ae038803cc2f60b4f6b7e5fef99f899e1c3419
AnuragPandey11 <91328139+AnuragPandey11@users.noreply.github.com>
2021-12-06 10:47:16 +0530

    Create task2_c.cpp

---
+++ Practical-11/task2_c.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+class A{
+    public:
+    int x;
+    protected:
+    int y;
+    private:
+    int z;
+};
+class B:protected A{
+  public:
+  void display()
+  {
+      x=10;
+      y=20;
+      cout<<"x=>"<<x<<" y=>"<<y;
+  }
+};
+int main()
+{
+    B obj;
+    //obj.x=30;
+    //obj.y=40;
+    obj.display();
+    return 0;
+}
